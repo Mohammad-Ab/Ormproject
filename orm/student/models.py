a19e7ea200647d0080b3f75986ffcd8bfed014f9
Mohammad-Ab <root.alpha1@gmail.com>
2023-07-18 13:25:26 +0330

    Write Queryset Using Filter Method and Q Function

--- orm/student/models.py
+++ orm/student/models.py
@@ -3,6 +3,31 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
     name = models.CharField(max_length=100)
+    description = models.TextField(default="This is a Student.")
+
+    def __str__(self):
+        return self.name
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(default="This is a Lesson.")
+
+    def __str__(self):
+        return self.name
+
+class Package(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(default="This is a Package.")
+
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    package = models.ForeignKey(Package,on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Lesson,on_delete=models.CASCADE)
+    students = models.ManyToManyField(Student)
+    name = models.CharField(max_length=100)
+    description = models.TextField(default="This is a Course")
 
     def __str__(self):
         return self.name
--- orm/student/views.py
+++ orm/student/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from .models import Student
+from .models import Student,Package,Lesson,Course
+from django.db.models import Q
 # Create your views here.
 def Index(request):
-    queryset = Student.objects.all()
+    #queryset = Student.objects.all()
+    #queryset = Student.objects.filter(course__lesson__name="Math")
+    #queryset = Package.objects.filter(course__lesson__name="Physic", course__students__name="Gisoo")
+    queryset = Package.objects.filter(Q(course__lesson__name="Physic") & Q(course__students__name="Gisoo"))
     print(queryset)
     return render(request,'student/index.html',{'students':queryset})
 
