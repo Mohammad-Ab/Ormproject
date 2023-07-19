ae469d816ba1284805bbf6151b5053944f8419e5
Mohammad-Ab <root.alpha1@gmail.com>
2023-07-19 11:35:54 +0330

    Ordering

--- orm/student/views.py
+++ orm/student/views.py
@@ -3,21 +3,31 @@ from .models import Student,Package,Lesson,Course,Teacher
 from django.db.models import Q
 # Create your views here.
 def Index(request):
-    #queryset = Student.objects.all()
-    #queryset = Student.objects.filter(course__lesson__name="Math")
-    #queryset = Package.objects.filter(course__lesson__name="Physic", course__students__name="Gisoo")
-    #queryset = Package.objects.filter(Q(course__lesson__name="Physic") & Q(course__students__name="Gisoo"))
-    #queryset = Package.objects.filter(course__students__name).exclude(name__in=['Gisoo','Sima'])
-    #queryset = Package.objects.filter(course__lesson__name="Math").values("name","course__name","course__description")
-    #queryset = Package.objects.filter(course__lesson__name="Math").values_list("name","course__name","course__description")
-    #queryset = Package.objects.filter(course__students__name="Gisoo").distinct() /* dont repeat Packages name*/
+    #here we use simple filter method and Q Function
+    '''
+        queryset = Student.objects.all()
+        queryset = Student.objects.filter(course__lesson__name="Math")
+        queryset = Package.objects.filter(course__lesson__name="Physic", course__students__name="Gisoo")
+        queryset = Package.objects.filter(Q(course__lesson__name="Physic") & Q(course__students__name="Gisoo"))
+    '''
     
-    #queryset = Package.objects.all().order_by('course__students__name','course__name').values('name','course__students__name','course__lesson__name')
+    #here we use exclude, values and values_list
+    '''
+        queryset = Package.objects.filter(course__students__name).exclude(name__in=['Gisoo','Sima'])
+        queryset = Package.objects.filter(course__lesson__name="Math").values("name","course__name","course__description")
+        queryset = Package.objects.filter(course__lesson__name="Math").values_list("name","course__name","course__description")
+    '''
+
+    #here we use order_by method and distinct to stop repeat some packages
+    '''
+        #queryset = Package.objects.filter(course__students__name="Gisoo").distinct() /* dont repeat Packages name*/
+        #queryset = Package.objects.all().order_by('course__students__name','course__name').values('name','course__students__name','course__lesson__name')
+    '''
+
     qs1=Student.objects.all().values_list("name","nationalcode")
     qs2=Teacher.objects.all().values_list("fullname","nationalcode")
-
     queryset = qs1.union(qs2)
-
     print(queryset)
+
     return render(request,'student/index.html',{'students':queryset})
 
